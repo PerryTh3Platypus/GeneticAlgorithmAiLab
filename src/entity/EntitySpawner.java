799e1b8cabbcd0652d7e02165a69e32e2accb597
OV3RDR1VE <forgotten_dead_knight@tutanota.com>
2024-01-12 14:53:27 +0200

    added an entity spawner

---
+++ src/entity/EntitySpawner.java
@@ -0,0 +1,33 @@
+package entity;
+
+import rng.RandomNumberGenerator;
+
+import java.util.ArrayList;
+
+public class EntitySpawner {
+    public static ArrayList<Entity> spawnEntities(int numOfEntities){
+        ArrayList<Entity> entities = new ArrayList<>();
+        for (int i = 0; i < numOfEntities; i++){
+            Entity entity = new Entity(5);
+            entity.setChromosome(generateRandomChromosome());
+            entities.add(entity);
+        }
+        return entities;
+    }
+
+    public static ArrayList<Integer> generateRandomChromosome(){
+        boolean isDone = false;
+
+        ArrayList<Integer> chromosome = new ArrayList<>();
+        while (!isDone){
+            int randomNumber = RandomNumberGenerator.generateNumber(0, 9);
+            if (!chromosome.contains(randomNumber))
+                chromosome.add(randomNumber);
+            else
+                continue;
+            if (chromosome.size() == 9)
+                isDone = true;
+        }
+        return chromosome;
+    }
+}
