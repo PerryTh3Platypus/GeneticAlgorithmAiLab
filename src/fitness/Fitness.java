055ff67b43f82638be05c6158dc84b24ee4a06e8
OV3RDR1VE <forgotten_dead_knight@tutanota.com>
2024-01-12 15:06:45 +0200

    updated Fitness class

--- src/fitness/Fitness.java
+++ src/fitness/Fitness.java
@@ -9,7 +9,10 @@ import java.util.Comparator;
 public class Fitness {
     public static Comparator<Entity> entityComparator = Comparator.comparing(Entity::getFitness).reversed();
     public static void calculateFitness(Entity entity, ArrayList<Integer> solution){
-        // 1 = worst fitness, 10 = perfect fitness
+        /* Calculates fitness. Function will look at the puzzle's tiles and will give
+        *  a higher fitness the more tiles are in their correct place.
+        *  1 = worst fitness (no tiles are where they should be),
+        *  10 = perfect fitness (all tiles are where they should be) */
         int fitness = 1;
         for (int i = 0; i < 9; i++){
             if (entity.getChromosome().get(i) == solution.get(i))
@@ -19,6 +22,7 @@ public class Fitness {
     }
 
     public static void sortByFitness(ArrayList<Entity> entities){
-        entities.sort(entityComparator);
+        // sorts entities in a list. The entities with higher fitness come first, kinda like having priority in a queue.
+        entities.sort(entityComparator.reversed());
     }
 }
