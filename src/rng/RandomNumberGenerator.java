2586ca51cfcc3277afda82b80d9c3623afd9cec8
OV3RDR1VE <forgotten_dead_knight@tutanota.com>
2024-01-12 18:24:24 +0200

    updated the random number generator

--- src/rng/RandomNumberGenerator.java
+++ src/rng/RandomNumberGenerator.java
@@ -6,6 +6,9 @@ public class RandomNumberGenerator {
     public static Random random = new Random();
 
     public static int generateNumber(int min, int max){
-        return random.nextInt((max - min) + 1) + min;
+        int bound = (max - min) + 1;
+        if (bound < 0)
+            bound = 0;
+        return random.nextInt(bound) + min;
     }
 }
